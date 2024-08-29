25a3c4b76a2b3ea0b44d72afc65f50cd9a25f1e0
Jhordan Huera <jhordanhuera2002@gmail.com>
2024-08-29 16:42:53 -0500

    programa que verifica la edad, si eres mayor edad o no

---
+++ calcular_edad.py
@@ -0,0 +1,15 @@
+
+def verificar_edad():
+    edad = int(input("Ingresa tu edad:"))
+    print("Tu edad es: ", edad," Por lo tanto: ")
+
+    if edad >= 18:
+        print("Bienvenido eres mayor de edad")
+    else:
+        print("No puedes ingresar no eres mayor de edad")
+    
+
+
+
+
+verificar_edad()
